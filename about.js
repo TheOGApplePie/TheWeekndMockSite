31bb0c4048bdb1a50d82a7e060fa4860e51a1437
Danial Ali <thedanial.ali@gmail.com>
2022-05-17 17:47:45 -0400

    All done, just added comments to about.js

--- about.js
+++ about.js
@@ -4,6 +4,10 @@ if(document.readyState == 'loading'){
     ready()
 }
 function ready(){
+    /**
+     * Fetch content from the Wikipedia API using the URL.
+     * Use Regular Expressions in along with the 'replace all' method to filter out unwanted data from the results json
+     */
     let url = 'https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=The_Weeknd&rvprop=content';
     fetch(url)
     .then(response => {
@@ -11,15 +15,26 @@ function ready(){
         .then(res =>{
             let pageId = Object.keys(res.query.pages)[0];
             var content = res.query.pages[pageId].revisions[0]['*'];
+
+            // Capture anything between one or more of '{' and '}'
             const regex1 = /({+.*}+}+)/g;
+
+            // Capture anything between one or more of '[' and ']'
             const regex2 = /\[+|\]+/g;
+
+            // Capture all '|' characters
             const regex3 = /\|/g;
+
+            // Capture anything that starts with 'File'
             const regex4 = /File.*\//g;
+
+            // Various indices to break up the text
             const index1 = content.indexOf("'''");
             const index2 = content.indexOf("==Early");
             const index3 = content.indexOf("==Career");
             const index4 = content.indexOf("===2012");
-
+            
+            // Breaking up the initial text from the Wikipedia API into smaller paragraphs and filtering unwanted text via regex
             let text = content.substring(index1, index2);
             text = text.replaceAll(regex1, "");
             text = text.replaceAll(regex2, "");
@@ -36,7 +51,7 @@ function ready(){
             text3 = text3.replaceAll(regex3, "/");
             text3 = text3.replaceAll(regex4, "");
 
-            console.log(content);
+            // Adding the text elements to the HTML as well as an image (Formatting HTML to copy from about.html)
             var textContentRow = document.createElement('div');
             var textItems = document.getElementsByClassName('inner-html-row')[0];
             textContentRow.classList.add('content-section');
